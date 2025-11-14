5add0384192e3111b5c97922f9c3116fc23294af
Ampflower 🌺 <git@ampflower.gay>
2025-11-14 06:36:59 -0800

    feat: more javap, now with more MethodHandle

--- src/test/java/dev/spiritstudios/mojank/meow/test/debug/DebugUtils.java
+++ src/test/java/dev/spiritstudios/mojank/meow/test/debug/DebugUtils.java
@@ -8,8 +8,41 @@ import org.jetbrains.java.decompiler.main.decompiler.ConsoleFileSaver;
 import org.objectweb.asm.ClassReader;
 import org.slf4j.Logger;
 
-public class DebugUtils {
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public final class DebugUtils {
 	private static final Logger logger = Util.logger();
+
+	private static final MethodHandle javap;
+
+	// Enjoy :3
+	static {
+		MethodHandle jp = MethodHandles.empty(MethodType.methodType(void.class, String[].class));
+		try {
+			final var lookup = MethodHandles.lookup();
+
+			final var clazz = Class.forName("com.sun.tools.javap.Main");
+			final var method = clazz.getMethod("run", String[].class, PrintWriter.class);
+
+			method.setAccessible(true);
+
+			jp = MethodHandles.insertArguments(lookup.unreflect(method), 1, new PrintWriter(System.out) {
+				@Override
+				public void close() {
+					// no-op
+				}
+			}).asVarargsCollector(String[].class);
+		} catch (Throwable t) {
+			logger.warn("Unable to load javap:", t);
+		}
+		javap = jp;
+	}
 
 	public static void debug(byte[] bytecode) {
 		DebugUtils.decompile(bytecode);
@@ -32,5 +65,26 @@ public class DebugUtils {
 		reader.accept(disassembler, ClassReader.SKIP_FRAMES);
 
 		logger.info(disassembler.output());
+	}
+
+
+	private static void disassembleWithJavap(byte[] bytecode) {
+		Path temp = null;
+		try {
+			temp = Files.createTempFile("meow", ".class");
+
+			Files.write(temp, bytecode);
+
+			javap.invoke("-l", "-p", "-c", temp.normalize().toString());
+		} catch (Throwable t) {
+			logger.warn("Unable to execute javap: ", t);
+		} finally {
+			try {
+				if (temp != null) {
+					Files.delete(temp);
+				}
+			} catch (IOException ignored) {
+			}
+		}
 	}
 }
